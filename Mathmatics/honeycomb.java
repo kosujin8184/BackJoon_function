97ae08ef81dab3dd68f916e1492e714356d14d14
kosujin <dus015@naver.com>
2021-06-06 19:09:02 +0900

    0606commit

--- Mathmatics/honeycomb.java
+++ Mathmatics/honeycomb.java
@@ -1,7 +1,34 @@
 package Mathmatics;
 
+import java.lang.*;
+import java.util.*;
+
+
+//어려움
 public class honeycomb {
     public static void main(String[] args){
-        
+
+        int honeycomb_cnt=1, des=2;
+        int N=0;
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+       
+            
+            if(N == 1){
+                System.out.println(N);
+            }
+            else{
+                while(des <= N){
+                    des = des + (6*honeycomb_cnt);
+                    honeycomb_cnt++;
+                }
+
+                System.out.println(honeycomb_cnt);
+            }
+    
+       
+
     }
 }
